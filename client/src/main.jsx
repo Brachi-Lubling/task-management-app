90d944b5a8a52510f77ab047b8d35554eeed1814
Brachi-Lubling <brachil5119@gmail.com>
2026-05-25 21:45:15 +0300

    update main.jsx to support store and providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+
+import { configureStore } from '@reduxjs/toolkit';
+import ProjectsSlice from './Store/ProjectsSlice.js';
+import UserSlice from './Store/UserSlice.js';
+import TasksSlise from './Store/TasksSlice.js'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: {
+    user: UserSlice,
+    projects: ProjectsSlice,
+    tasks: TasksSlise
+  }
+})
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
+  //</StrictMode>,
 )
