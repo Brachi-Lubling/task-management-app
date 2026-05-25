a0a13d18822cbefaf6df19c0aecc04b14344c39a
Brachi-Lubling <brachil5119@gmail.com>
2026-05-25 21:10:02 +0300

    feat: add project routs

---
+++ server/routes/projects.route.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { getProjects, getProject, updateProject, addProject, deleteProject } from "../controllers/projects.controlles.js";
+
+const router = express.Router()
+
+router.get('/', getProjects)
+router.get('/:id', getProject)
+router.put('/:id', updateProject)
+router.post('/', addProject)
+router.delete('/:id', deleteProject)
+
+export default router
